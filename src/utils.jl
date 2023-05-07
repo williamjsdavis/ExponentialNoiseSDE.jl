8433a67e0cfab4b3a0c26f6815edccc537aa7ce3
williamjsdavis <williamjsdavis@berkeley.edu>
2023-05-07 13:15:43 -0700

    Changes to movmean

--- src/utils.jl
+++ src/utils.jl
@@ -1,7 +1,6 @@
 # Utility and statistic functions
 
-#moving_average(vs,n) = [sum(@view vs[i:(i+n-1)])/n for i in 1:(length(vs)-(n-1))]
-
+# Moving average, inspired my MATLAB
 """
 M = movmean(X,n) returns an array of local k-point mean values, where each mean is calculated over a sliding window of length k across neighboring elements of A. 
 When k is odd, the window is centered about the element in the current position. 
@@ -36,17 +35,6 @@ function movmean_even!(out,x,k,N)
     for i in 1:N
         imin = max(1,i-j)
         imax = min(N,i+j-1)
-        @show (imin,imax)
         out[i] = sum(view(x,imin:imax))/(imax-imin+1)
     end
-end
-
-## To add to tests
-@testset "Statistics and utility functions" begin
-    @test movmean([1,2,3,4,5],3) .== [1.5,2.0,3.0,4.0,4.5]
-    @test movmean([1,2,3,4,5],2) .== [1.0,1.5,2.5,3.5,4.5]
-    @test movmean([1,2,3,4,5],4) .== [1.5,2.0,2.5,3.5,4.0]
-    @test movmean([1,2,3,4],3) .== [1.5,2.0,3.0,3.5]
-    @test movmean([10,2,3,2,4,8,21],4) .== [6.0,5.0,4.25,2.75,4.25,8.75,11.0]
-    @test movmean([9,2,4,3,2,7,21],5) .== [5.0,4.5,4.0,3.6,4.7,8.25,10.0]
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,22 @@ using DelimitedFiles
 using Test
 include("small_data_test.jl")
 
+## Utilities
+@testset "Statistics and utility functions" begin
+    @test all(ExponentialNoiseSDE.movmean([1,2,3,4,5],3) .== [1.5,2.0,3.0,4.0,4.5])
+    @test all(ExponentialNoiseSDE.movmean([1,2,3,4,5],2) .== [1.0,1.5,2.5,3.5,4.5])
+    @test all(ExponentialNoiseSDE.movmean([1,2,3,4,5],4) .== [1.5,2.0,2.5,3.5,4.0])
+    @test all(ExponentialNoiseSDE.movmean([1,2,3,4],3) .== [1.5,2.0,3.0,3.5])
+    @test all(ExponentialNoiseSDE.movmean([10,2,3,2,4,8,21],4) .== [6.0,5.0,4.25,2.75,4.25,8.75,11.0])
+    @test all(ExponentialNoiseSDE.movmean([9,2,4,3,2,7,21],5) .== [5.0,4.5,4.0,3.6,7.4,8.25,10.0])
+end
 
 ## Small data test
 small_data_test()
 
 ## Validate calculations (against MATLAB calculations)
 
-X_data = DelimitedFiles.readdlm("./data/exampleData.txt")[:]
+X_data = readdlm("./data/exampleData.txt")[:]
 
 obs = Observation(
     X_data,
@@ -45,8 +54,3 @@ modelSettings = ModelEstimateSettings()
 # Estimating
 modelEstimate = estimate_model(conditionalMoments, modelSettings)
 
-## Load MATLAB calculations
-
-xEvalPointsMATLAB = DelimitedFiles.readdlm("./data/evalPointsMATLAB.txt")[:]
-
-@show xEvalPointsMATLAB
